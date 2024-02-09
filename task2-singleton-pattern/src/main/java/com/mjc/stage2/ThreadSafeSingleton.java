2420b1c6501026dba2cd5fba12442cc7393991e1
VSvetlakov <116279684+VSvetlakov@users.noreply.github.com>
2024-02-10 00:23:59 +0400

    v.5

--- task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
+++ task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
@@ -4,7 +4,7 @@ public class ThreadSafeSingleton {
 
     private static volatile ThreadSafeSingleton instance;
 
-    public static ThreadSafeSingleton GetInstance() {
+    public static ThreadSafeSingleton getInstance() {
 
         if (instance == null) {
             synchronized (ThreadSafeSingleton.class) {
